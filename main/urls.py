026e40d9b0d06783ee95f6739656366a6db2b0bd
THANIKKUL PROMSREE <81483735+thanikkul@users.noreply.github.com>
2022-10-04 02:33:40 +0700

    Add files via upload

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import (
+    list_view,
+    client 
+    # //Work exercise ,Insert code here    
+)
+
+app_name = 'main'
+urlpatterns = [
+    path('', list_view, name='home-list'),
+    path('join-now/',client, name= 'hangout'),
+
+]
---
+++ static/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var socket = io('http://localhost:3000');
+    socket.on('hi', function(msg) {
+      Swal.fire({
+        title: 'Success!',
+        text: msg,
+        icon: 'success',
+        confirmButtonText: 'Okay'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          socket.on("random", (data) => {
+            AppendData(data);
+          });
+        } 
+      })
+    });
+    $('#btnSendMsg').click( function(){
+      // url from where we want to get the data
+       console.log("SendMsg");
+  
+       data_input = { "msg": $('#txtMessage').val(), "name": $('#txtName').val()};
+  
+       socket.emit('joidRoom', data_input, function (data) {
+              console.log('sent message to server!');
+              console.log(data);
+              //
+              Swal.fire({
+                  title: 'ขอบคุณที่บอกฉัน',
+                  text: data,
+              })
+      });
+    });
+  });
+  
+  
+  
+  function AppendData(val){
+    $("#listUser").append('<a href="#" class="list-group-item list-group-item-action"> Random : '+ val +'</a>');
+  }
+  //Work exercise,Insert code here
